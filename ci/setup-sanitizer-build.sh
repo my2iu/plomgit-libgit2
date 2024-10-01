9fcf83792b47f79ec7ead9c7b14e95ef316fecd1
Edward Thomson <ethomson@edwardthomson.com>
2024-10-01 23:53:04 +0100

    Update msan header file to be c90 compliant

--- ci/setup-sanitizer-build.sh
+++ ci/setup-sanitizer-build.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+if [ -f /usr/local/msan/include/mbedtls/sha1.h ]; then
+	sudo sed -i -e 's/\/\/\(.*\)/\/\*\1 \*\//g' /usr/local/msan/include/mbedtls/sha1.h
+fi
+
 # Linux updated its ASLR randomization in a way that is incompatible with
 # TSAN. See https://github.com/google/sanitizers/issues/1716
 sudo sysctl vm.mmap_rnd_bits=28
